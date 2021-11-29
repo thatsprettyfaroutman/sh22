0365639d8cc1f9f08a1c7eb99afd2e86303de457
thatsprettyfaroutman <viljami.anttonen@gmail.com>
2021-11-29 16:36:16 +0200

    feat: swingy frame seconds and in view check

--- components/SwingyFrame/index.js
+++ components/SwingyFrame/index.js
@@ -1,13 +1,13 @@
 import { useMemo } from 'react'
 import styled from 'styled-components'
 import { a } from 'react-spring'
+import { useInView } from 'react-intersection-observer'
 import { useInfiniteSpringContext } from '@contexts/infiniteSpring'
 
 const StyledSwingyFrame = styled(a.div)`
   transform-origin: 50% 10px;
 
-  ::after {
-    content: ' ';
+  > div {
     position: absolute;
     top: 100%;
     left: 50%;
@@ -15,25 +15,39 @@ const StyledSwingyFrame = styled(a.div)`
     height: 160px;
     margin-left: -55px;
     margin-top: -20px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     background-color: #f0f;
+    color: #fff;
+    font-size: 300%;
   }
 `
 
 export const SwingyFrame = ({ ...restProps }) => {
-  const { infiniteSpring } = useInfiniteSpringContext()
+  const { ref, inView } = useInView()
+  const { infiniteSpring, secondsPassed } = useInfiniteSpringContext()
 
   const p = useMemo(
-    () => infiniteSpring.time.to((t) => (t % 2000) / 2000),
-    [infiniteSpring]
+    () =>
+      inView ? infiniteSpring.time.to((t) => (t % 2000) / 2000) : undefined,
+    [infiniteSpring, inView]
   )
 
   return (
     <StyledSwingyFrame
-      style={{
-        rotate: p.to((p) => Math.sin(p * Math.PI * 2) * 20),
-      }}
+      ref={ref}
+      {...restProps}
+      style={
+        p && {
+          rotate: p.to((p) => Math.sin(p * Math.PI * 2) * 20),
+        }
+      }
     >
       <img src="/images/julesframe.svg" alt="Jules" />
+      <div>{secondsPassed % 2}</div>
     </StyledSwingyFrame>
   )
 }
